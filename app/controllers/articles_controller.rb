a74ca026654b17978ab6e489f0d708411f46681c
Glenn Langdon <glennlangdon@gmail.com>
2015-12-15 16:34:40 -0600

    completion of blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: 'Glenn', password: 'password',
+  except: [:index, :show]
+
   def index
     @articles = Article.all
   end
